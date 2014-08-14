b81e5d32a4f5123bb0497d5c6b35ca91cdc76e90
Tô Thành Công <tcvn1985@gmail.com>
2014-08-14 11:40:56 +0700

    Script dùng để kiểm tra phiên bản openstack

---
+++ kt-phienban-openstack.py
@@ -0,0 +1,9 @@
+################################################
+## Chuong trinh kiem tra phien ban OpenStack
+## VDC-IT
+################################################
+
+#!/bin/python
+
+from nova import version
+print version.version_string()
