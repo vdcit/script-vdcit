cee97a503cc649d34b8a4159df6c202a65740688
Tô Thành Công <tcvn1985@gmail.com>
2014-08-18 08:50:58 +0700

    Update kt-phienban-openstack.py

--- vdcit-script/kt-phienban-openstack.py
+++ vdcit-script/kt-phienban-openstack.py
@@ -1,9 +1,10 @@
 ################################################
 ## Chuong trinh kiem tra phien ban OpenStack
 ## VDC-IT
+## Ngày cập nhật: 18/08/2014
+###############################################
 ## Cách thực hiện
 # python kt-phienban-openstack.py
-## Ngày cập nhật: 18/08/2014
 ################################################
 
 #!/bin/python
